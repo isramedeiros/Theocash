4e593ef9f1a754ebab8d49de7b061bf391fd2030
Israel Medeiros <israelmfreitas12@gmail.com>
2026-03-11 17:26:03 -0300

    Initial commit: Configuração do Docker e Spring Boot

---
+++ src/main/java/br/com/fiap/Theocash/TheocashApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Theocash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheocashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheocashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/Theocash/TheocashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Theocash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheocashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
